9762b804b6aea62f8619f92a1e61861af8777205
iig01 <iisgg20@gmail.com>
2022-08-01 23:06:05 +0500

    API key was changed

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = 'fe3162e9-edb3-48a1-aecb-129abd87a648';
+export const KEY = 'd8ed9540-c0a3-4f61-824a-fae2c8c13fca';
 export const MODAL_CLOSE_SEC = 2.5;
